6f1f3ad0b23be8498a5d5e128f3c1232944a2f97
Clara Herkert <herkertclara@gmail.com>
2023-08-17 16:04:01 +0200

    ok

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,30 @@
+class BookmarksController < ApplicationController
+before_action :set_movie, only: %i[new create]
+
+  def new
+    #@list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    @bookmark.save!
+    redirect_to list_path(@list)
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+  def set_movie
+    @list = List.find(params[:list_id])
+  end
+end
